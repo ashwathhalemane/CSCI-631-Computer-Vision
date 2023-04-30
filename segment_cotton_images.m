1697e2c2c7e7693623dee3450884eafe13699879
Ashwath <ashwath.1si13te012@gmail.com>
2023-04-30 12:03:45 -0400

    save image with names updated

--- segment_cotton_images.m
+++ segment_cotton_images.m
@@ -2,35 +2,34 @@ function segment_cotton_images()
   
 save_files_with_new_name()
     function save_files_with_new_name( )
-%       change your path here    
+        %       change your path here to point to tif images 
+
         file_list = dir('D:\RIT-Spring-2023\Computer Vision\PROJECT\cotton images and labels\cotton images\*.tif');
-        
+        % path where you save preprocessed images
+        path = 'D:\RIT-Spring-2023\Computer Vision\PROJECT\cotton images and labels\cotton images\';
+
         for counter = 1:length(file_list)
             fn = file_list(counter).name;
-            
-            fprintf(fn + "\n");
-            
             [filepath, name, ~] = fileparts(fn);
             name_without_extension = fullfile(filepath, name);
-            fprintf(name_without_extension + "\n")
             updated_file_name = regexp(name_without_extension, '\d+$', 'match'); % extract the last characters which are numbers
-            disp(updated_file_name)
             new_filename = string(strcat(updated_file_name, '.tif'));
-            disp(new_filename);
-            
-
+           
             new_folder_path = 'D:\RIT-Spring-2023\Computer Vision\PROJECT\prepro'; 
+            
+            if ~exist(new_folder_path, 'dir')
+                mkdir(new_folder_path);
+            end
 
-
-            image_to_read = imread(fn);
+            image_to_read = imread(string(strcat(path, fn)));
             resizing_tiff = image_to_read(:,:,1:3);
-            disp(size(image_to_read))
-%             pre-processing steps comes here
-%             im is the updated image
-            full = fullfile(new_folder_path, new_filename);
-            disp(full);
+            %   pre-processing steps comes here
+
+            %   im is the updated image
+
+            fullpath_with_imagename = fullfile(new_folder_path, new_filename);
             % Set the compression to LZW
-            imwrite(resizing_tiff, full, 'tif');
+            imwrite(resizing_tiff, fullpath_with_imagename, 'tif');
         end
 
     
